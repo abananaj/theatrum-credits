9b9295e647f487e88068529387ffe9b5fbf6e257
abananaj <anna@theatrum.dev>
2026-09-08 02:04:45 -0700

    deploy: prepare branch (removed src/, docs, and build config)

--- src/blocks/artist-credits/view.js
+++
@@ -1,9 +0,0 @@
-/**
- * Front-end JS for this block, enqueued via block.json's `viewScript`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
- */
-
-/* eslint-disable no-console */
-console.log('Hello World! (from artist-credits view.js)');
-/* eslint-enable no-console */
--- src/blocks/production-credits/view.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-console */
-console.log('Hello World! (from production-credits view.js)');
-/* eslint-enable no-console */
